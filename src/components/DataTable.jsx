18956c55773572ab31b0de99caf8a3a8d8997336
0seaS <00sea55@gmail.com>
2023-09-28 22:51:11 -0400

    desarrollo del filtro por fechas

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -6,10 +6,12 @@ import "./styles/DataTable.css"
 const DataTable = ({setEditar}) => {
 
     const {dataApi, getData, startDB} = useFetch()
-    const [selectSucursal, setSelectSucursal] = useState("Todos")
+    const [selectSucursal, setSelectSucursal] = useState("Sacaba")
     const [showData, setShowData] = useState()
     const [totalCajas, setTotalCajas] = useState(0)
     const [totalGastos, setTotalGastos] = useState(0)
+    const [fromDateValue, setFromDateValue] = useState("2023-08-01")
+    const [toDateValue, setToDateValue] = useState(new Date().toISOString().slice(0, 10))
 
     useEffect(() => {
         startDB()
@@ -20,28 +22,22 @@ const DataTable = ({setEditar}) => {
         let show
 
         if (selectSucursal === "Villa") {
-            show = dataApi.filter(data => data.sucursal == "villa").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
-            setShowData(show)
+            show = dataApi?.filter(data => data.sucursal == "villa").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
         }
         else if(selectSucursal === "Chimore") {
-            show = dataApi.filter(data => data.sucursal == "chimore").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
-            setShowData(show)
+            show = dataApi?.filter(data => data.sucursal == "chimore").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
         }
         else if(selectSucursal === "Sacaba") {
-            show = dataApi.filter(data => data.sucursal == "sacaba").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
-            setShowData(show)
+            show = dataApi?.filter(data => data.sucursal == "sacaba").sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime())
         }
-        else{
-            setShowData(dataApi?.sort((a, b) => new Date(b.fecha).getTime() - new Date(a.fecha).getTime()))
-        }
-    }, [dataApi, selectSucursal])
+        show = show?.filter(data => new Date(data.fecha).getTime() <= new Date(toDateValue).getTime() && new Date(data.fecha).getTime() >= new Date(fromDateValue).getTime())
+        setShowData(show)
+    }, [dataApi, selectSucursal, toDateValue, fromDateValue])
 
     useEffect(() => {
         setTotalCajas(showData?.reduce((acc, cur) => acc + Number(cur.caja), 0))
         setTotalGastos(showData?.reduce((acc, cur) => acc + cur.gastos.reduce((acc, cur) => acc + cur.precio, 0), 0))
     }, [showData])
-
-
 
     function handleSacaba(){
         setSelectSucursal("Sacaba")
@@ -53,10 +49,19 @@ const DataTable = ({setEditar}) => {
         setSelectSucursal("Chimore")
     }
 
+    function setDatesValues(){
+        setToDateValue(document.getElementById("toDate").value)
+        setFromDateValue(document.getElementById("fromDate").value)
+    }
+
+    console.log(toDateValue)
+    console.log(fromDateValue)
+
   return (
     <article className="data__container">
         <div className="btn__select-container"><button className="btn__select" onClick={handleSacaba}>Sacaba</button><button className="btn__select" onClick={handleVilla}>Villa Tunari</button><button className="btn__select" onClick={handleChimore}>Chimore</button></div>
-        
+        <div className="filterDate"><span>Desde: </span><input type="date" id="fromDate" value={fromDateValue} onChange={setDatesValues}/> </div>
+        <div className="filterDate"><span>Hasta: </span><input type="date" id="toDate" value={toDateValue} onChange={setDatesValues}/></div>
         <header>
             <h2>Cuentas de {selectSucursal}</h2>
         </header>
@@ -79,8 +84,8 @@ const DataTable = ({setEditar}) => {
                         />
                     ))
                 }
-                <div className="total__cajas">Total Gastos: {totalGastos} Bs.</div>
-                <div className="total__cajas">Total en Caja: {totalCajas} Bs.</div>
+                <div className="total__cajas">Total Gastos: {totalGastos?.toFixed(2)} Bs.</div>
+                <div className="total__cajas">Total en Caja: {totalCajas?.toFixed(2)} Bs.</div>
             </div>
         </section>
     </article>
